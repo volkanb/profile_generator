e1709cc620b8109cb82ffa55146c8eeb0bf4e399
Volkan Benli <volkanbenli92@gmail.com>
2021-09-22 02:46:48 +0000

    Adds survey logic

--- survey.js
+++ survey.js
@@ -5,8 +5,26 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-rl.question('What do you think of Node.js? ', (answer) => {
-  console.log(`Thank you for your valuable feedback: ${answer}`);
+const answers = [];
+const questions = [
+  "What's your name? Nicknames are also acceptable :)",
+  "What's an activity you like doing?",
+  "What do you listen to while doing that?",
+  "Which meal is your favourite (eg: dinner, brunch, etc.)",
+  "What's your favourite thing to eat for that meal?",
+  "Which sport is your absolute favourite?",
+  "What is your superpower? In a few words, tell us what you are amazing at!"
+];
 
-  rl.close();
-});
+const askQuestions = (x) => {
+  rl.question(questions[x], (answer) => {
+    answers.push(answer);
+    if (x + 1 < questions.length) {
+      askQuestions(x + 1);
+    } else {
+      rl.close();
+      console.log(`\nANSWERS: ${answers}`);
+    }
+  });
+};
+askQuestions(0);
